f9da5d8c851713e0bc7fc64eb57726d0fa901416
Suya6021 <smangaj@jspmrscoe.edu.in>
2024-07-08 22:15:54 +0530

    feat:Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Routes/Home";
+import JoinUs from "./Routes/JoinUs";
 
 const App = () => {
   return (
     <>
+    <div className="h-[100vh]">
       <Routes>
         <Route path="/" Component={Home} />
-        <Route path="/Login" />
+        <Route path="/Login" Component={JoinUs} />
         <Route path="/FootPrints" />
         <Route path="/Resources" />
         <Route path="/Events" />
         <Route path="/ContextUs" />
       </Routes>
+      </div>
     </>
   );
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import Nav from "./Components/Nav.tsx";
+import Footer from "./Components/Footer.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Nav/>
       <App />
+      <Footer/>
     </BrowserRouter>
   </React.StrictMode>
 );
